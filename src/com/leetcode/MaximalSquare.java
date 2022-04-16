a82d61ab04a564b50367f6987db4891e8f03e0b9
thirumalain <thiru.malai.n@gmail.com>
2022-04-16 15:09:10 -0500

    inprogress

---
+++ src/com/leetcode/MaximalSquare.java
@@ -0,0 +1,52 @@
+package com.leetcode;
+
+/**
+ * Given an m x n binary matrix filled with 0's and 1's, find the largest square containing only 1's and return its area.
+
+ 
+
+Example 1:
+
+
+Input: matrix = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]
+Output: 4
+Example 2:
+
+
+Input: matrix = [["0","1"],["1","0"]]
+Output: 1
+Example 3:
+
+Input: matrix = [["0"]]
+Output: 0
+ 
+
+Constraints:
+
+m == matrix.length
+n == matrix[i].length
+1 <= m, n <= 300
+matrix[i][j] is '0' or '1'.
+Accepted
+484,292
+Submissions
+1,116,760
+
+ * @author thiru
+ *
+ */
+
+public class MaximalSquare {
+	
+	public int maximalSquare(char[][] matrix) {
+		int max= 0;
+		
+        
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
